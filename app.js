e049ca162ee88f51fb0a73bdb9d04fb9c995ce6b
Chelsea Tarantine <asiajonesCB5@MacBook-Air-29.local>
2019-08-07 19:46:47 -0400

    modal and background changes

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 console.log("ello love")
 
-document.querySelector("right").addEventListener("click",)
 
 
-// 1. when card is click, modal containing question and answer appear
+//close Q&A modal
+document.querySelector(".close").addEventListener("click", function(){
+    document.querySelector(".modal").style.display ="none"
+})
+
+//display Q&A
+document.querySelector("right").addEventListener("click",function(){
+
+})
+
+
+// 1. when card is clicked, modal containing question and answer appear
 // 2. modal populates with info from array
 // 3. modal clears info when closed
 // 4. answer choice click add and subtracts listed point value
 // 5. value displays in proper places and keeps running total
-// 6. card appearance changes when end of array reached
+// 6. card appearance changes when end of array reached (display:none)
