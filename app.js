8d0224ca30a2dcb125173ccbb1e8d97139023f58
Chelsea Tarantine <asiajonesCB5@MacBook-Air-29.local>
2019-08-07 16:41:32 -0400

    beginning work on modal

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("ello love")
+
+document.querySelector("right").addEventListener("click",)
+
+
+// 1. when card is click, modal containing question and answer appear
+// 2. modal populates with info from array
+// 3. modal clears info when closed
+// 4. answer choice click add and subtracts listed point value
+// 5. value displays in proper places and keeps running total
+// 6. card appearance changes when end of array reached
