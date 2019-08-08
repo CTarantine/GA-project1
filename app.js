6173a7fd1f050fb10862e384a2ee1e2dbaf1562a
Chelsea Tarantine <asiajonesCB5@MacBook-Air-29.local>
2019-08-08 09:55:19 -0400

    javascript brainstorming

--- app.js
+++ app.js
@@ -19,3 +19,28 @@ document.querySelector("right").addEventListener("click",function(){
 // 4. answer choice click add and subtracts listed point value
 // 5. value displays in proper places and keeps running total
 // 6. card appearance changes when end of array reached (display:none)
+
+document.querySelector("questionbox").innerHTML=[];
+document.querySelector("a1").innerHTML=[];
+document.querySelector("a2").innerHTML=[];
+document.querySelector("a3").innerHTML=[];
+document.querySelector("a4").innerHTML=[];
+
+let question = ["blue"]
+let a = []
+let c = []
+let d = []
+let e = []
+
+
+sampleQuestion1
+sampleAnswer1
+sampleAnswer1
+sampleAnswer1
+sampleAnswer1
+
+sampleQuestion2
+sampleAnswer2
+sampleAnswer2
+sampleAnswer2
+sampleAnswer2
