909d48419b4ca7961d5a0217de7f607ba65fb733
Chelsea Tarantine <asiajonesCB5@MacBook-Air-29.local>
2019-08-09 11:25:23 -0400

    js error

--- app.js
+++ app.js
@@ -1,59 +1,57 @@
 console.log("ello love")
 
-
-
 //close Q&A modal
 document.querySelector(".close").addEventListener("click", function () {
     document.querySelector(".modal").style.display = "none"
 })
 
+let question = [];
+let answerA = [];
+let answerB = [];
+let answerC = [];
+let answerD = [];
+let correct = [];
 
-let i=0;
-//display Q&A
-document.querySelector("#right").addEventListener("click", function(x){
-    // console.log("working")
-    document.querySelector(".modal").style.display = "block";
-    let question = ["sampleQuestion1", "sampleQuestion2","sampleQuestion3"];
-    // let answerA = ["sampleAnswer1", "sampleAnswerI","sampleAnsweri"];
-    // let answerB = ["sampleAnswer2", "sampleAnswerII","sampleAnswerii"];
-    // let answerC = ["sampleAnswer3", "sampleAnswerIII","sampleAnsweriii"];
-    // let answerD = ["sampleAnswer4", "sampleAnswerIV","sampleAnsweriv"];
-    
-    
-    if (i < question.length){
-        document.querySelector("#questionbox").innerHTML = question[i];
-        i++;
-    }
-    else {
-        document.querySelector("#right").style.color = "black";
-    }
-    // for (let i = 0; i < answerA.length; i++) {
-    //     document.querySelector("#a1").innerHTML = answerA[i];
-        
-    // }
-    // for (let i = 0; i < answerB.length; i++) {
-    //     document.querySelector("#a2").innerHTML = answerB[i];
-        
-    // }
-    // for (let i = 0; i < answerC.length; i++) {
-    //     document.querySelector("#a3").innerHTML = answerC[i];
-    // }
-    // for (let i = 0; i < answerD.length; i++) {
-    //     document.querySelector("#a4").innerHTML = answerD[i];
-    // }
+//array items
+let i = 0;
 
-})
+//keeps track of score
+let score = parseFloat(document.querySelector(".score").innerHTML);
 
+    //display Q&A
+    document.querySelector("#right").addEventListener("click", function () {
+        // console.log("working")
+        document.querySelector(".modal").style.display = "block";
+        question = ["Molecule made up of 2 hydrogen atoms and 1 oxygen atom", "sampleQuestion2", "sampleQuestion3"];
+        answerA = ["What is earth?", "sampleAnswerI", "sampleAnsweri"];
+        answerB = ["What is water?", "sampleAnswerII", "sampleAnswerii"];
+        answerC = ["What is fire?", "sampleAnswerIII", "sampleAnsweriii"];
+        answerD = ["What is earth?", "sampleAnswerIV", "sampleAnsweriv"];
+        correct = ["What is water?", "sampleAnswerI", "sampleAnsweriv"];
+        points = parseFloat("100");
 
-// 1. when card is clicked, modal containing question and answer appear
-// 2. modal populates with info from array
-
-// 4. answer choice click add and subtracts listed point value
-// 5. value displays in proper places and keeps running total
-// 6. card appearance changes when end of array reached (display:none)
-
-
-
-
-
-
+            if (i < question.length && i < answerA.length && i < answerB.length && i < answerC.length && i < answerD.length && i < correct.length) {
+                document.querySelector("#questionbox").innerHTML = question[i];
+                document.querySelector("#a1").innerHTML = answerA[i];
+                document.querySelector("#a2").innerHTML = answerB[i];
+                document.querySelector("#a3").innerHTML = answerC[i];
+                document.querySelector("#a4").innerHTML = answerD[i];
+                correct[i];
+                i++;
+            }
+            // else {
+            //     document.querySelector(".modal").style.display = "none";
+            //     document.querySelector("#right").style.color = "black";
+            // }
+            let choices = document.querySelectorAll(".answerbox")
+        for (let x = 0; x < choices.length; x++) {
+            choices[x].addEventListener("click", function () {
+                if (answerA[i] === correct[i]) {
+                    console.log(score + points)
+                }
+                else {
+                    console.log(score - points)
+                }
+            })
+            
+    )}
---
+++ readme.md
@@ -0,0 +1,1 @@
+place a github pages link in here
