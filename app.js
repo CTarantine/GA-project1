90357d4a77b5a8a998901d6b86263c0f114868e3
Chelsea Tarantine <asiajonesCB5@MacBook-Air-29.local>
2019-08-10 19:02:04 -0400

    debugging

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ let answerD = [];
 let correct = [];
 
 //array items
-let i = 0;
 let a = 0;
 let b = 0;
 let c = 0;;
@@ -22,13 +21,13 @@ let d = 0;;
 //keeps track of score
 let score = parseFloat(document.querySelector(".score").innerHTML);
 
-let choices = document.querySelectorAll(".answerbox")
+const choices= document.querySelectorAll(".answerbox")
 
 
 //display Q&A
 document.querySelector("#right").addEventListener("click", function () {
     // console.log("working")
-    runSequence(a)
+    document.querySelector(".modal").style.display = "block";
     question = ["sampleQuestion1", "sampleQuestion2", "sampleQuestion3"];
     answerA = ["sampleAnswer1", "sampleAnswerI", "sampleAnsweri"];
     answerB = ["What is?", "sampleAnswerII", "sampleAnswerii"];
@@ -45,62 +44,87 @@ document.querySelector("#right").addEventListener("click", function () {
         document.querySelector("#a4").innerHTML = answerD[a];
         correct[a];
     }
-    a++
     if (a >= question.length && a >= answerA.length && a >= answerB.length && a >= answerC.length && a >= answerD.length && a >= correct.length) {
         document.querySelector("#right").style.color = "black";
+        document.querySelector("#right").style.opacity = "0.6";
     }
 })
+for (let x = 0; x < choices.length; x++) {
+    choices[x].addEventListener("click", function (event) {
+        const selected = event.target.innerHTML
+        console.log(correct[a])
+        console.log(selected)
+        if (selected === correct[a]) {
+            console.log(score + points);
+            score = score + points
+        }
+        else {
+            console.log(score - points);
+            score = score - points
+        }
+        document.querySelector(".score").innerHTML = score;
+        document.querySelector(".modal").style.display = "none";
+        a++;
+        if (a >= question.length && a >= answerA.length && a >= answerB.length && a >= answerC.length && a >= answerD.length && a >= correct.length) {
+            document.querySelector("#right").style.color = "black";
+            document.querySelector("#right").style.opacity = "0.6";
+            document.querySelector("#right").style.border = "solid black";
+            document.querySelector("#right").addEventListener("click", function () {
+                document.querySelector(".modal").style.display = "none"
+            })
+        }
+    })
+}
 
-document.querySelector("#bottom").addEventListener("click", function () {
-    runSequence(b)
-    question = ["Molecule made up of 2 hydrogen atoms and 1 oxygen atom", "sampleQuestion2", "sampleQuestion3"];
-    answerA = ["What is earth?", "sampleAnswerI", "sampleAnsweri"];
-    answerB = ["What is water?", "sampleAnswerII", "sampleAnswerii"];
-    answerC = ["What is fire?", "sampleAnswerIII", "sampleAnsweriii"];
-    answerD = ["What is earth?", "sampleAnswerIV", "sampleAnsweriv"];
-    correct = ["What is water?", "sampleAnswerI", "sampleAnsweriv"];
-    points = parseFloat("500");
+// document.querySelector("#bottom").addEventListener("click", function () {
+//     document.querySelector(".modal").style.display = "block";
+//     question = ["Molecule made up of 2 hydrogen atoms and 1 oxygen atom", "sampleQuestion2", "sampleQuestion3"];
+//     answerA = ["What is earth?", "sampleAnswerI", "sampleAnsweri"];
+//     answerB = ["What is water?", "sampleAnswerII", "sampleAnswerii"];
+//     answerC = ["What is fire?", "sampleAnswerIII", "sampleAnsweriii"];
+//     answerD = ["What is earth?", "sampleAnswerIV", "sampleAnsweriv"];
+//     correct = ["What is water?", "sampleAnswerI", "sampleAnsweriv"];
+//     points = parseFloat("500");
 
-    if (b < question.length && b < answerA.length && b < answerB.length && b < answerC.length && b < answerD.length && b < correct.length) {
-        document.querySelector("#questionbox").innerHTML = question[b];
-        document.querySelector("#a1").innerHTML = answerA[b];
-        document.querySelector("#a2").innerHTML = answerB[b];
-        document.querySelector("#a3").innerHTML = answerC[b];
-        document.querySelector("#a4").innerHTML = answerD[b];
-        correct[b];
-    }
-    b++
-    if (b >= question.length && b >= answerA.length && b >= answerB.length && b >= answerC.length && b >= answerD.length && b >= correct.length) {
-        document.querySelector("#bottom").style.color = "black";
-    }
-})
+//     if (b < question.length && b < answerA.length && b < answerB.length && b < answerC.length && b < answerD.length && b < correct.length) {
+//         document.querySelector("#questionbox").innerHTML = question[b];
+//         document.querySelector("#a1").innerHTML = answerA[b];
+//         document.querySelector("#a2").innerHTML = answerB[b];
+//         document.querySelector("#a3").innerHTML = answerC[b];
+//         document.querySelector("#a4").innerHTML = answerD[b];
+//         correct[b];
+//     }
+//     if (b >= question.length && b >= answerA.length && b >= answerB.length && b >= answerC.length && b >= answerD.length && b >= correct.length) {
+//         document.querySelector("#bottom").style.color = "black";
+//         document.querySelector("#bottom").style.opacity = "0.6";
+//     }
+// })
 
-document.querySelector("#left").addEventListener("click", function () {
-    runSequence(c)
-    question = ["sampleQuestion1", "sampleQuestion2", "sampleQuestion3"];
-    answerA = ["What is?", "sampleAnswerI", "sampleAnsweri"];
-    answerB = ["What is?", "sampleAnswerII", "sampleAnswerii"];
-    answerC = ["What is?", "sampleAnswerIII", "sampleAnsweriii"];
-    answerD = ["What is?", "sampleAnswerIV", "sampleAnsweriv"];
-    correct = ["What is?", "sampleAnswerI", "sampleAnsweriv"];
-    points = parseFloat("700");
+// document.querySelector("#left").addEventListener("click", function () {
+//     document.querySelector(".modal").style.display = "block";
+//     question = ["sampleQuestion1", "sampleQuestion2", "sampleQuestion3"];
+//     answerA = ["What is?", "sampleAnswerI", "sampleAnsweri"];
+//     answerB = ["What is?", "sampleAnswerII", "sampleAnswerii"];
+//     answerC = ["What is?", "sampleAnswerIII", "sampleAnsweriii"];
+//     answerD = ["What is?", "sampleAnswerIV", "sampleAnsweriv"];
+//     correct = ["What is?", "sampleAnswerI", "sampleAnsweriv"];
+//     points = parseFloat("700");
 
-    if (c < question.length && c < answerA.length && c < answerB.length && c < answerC.length && c < answerD.length && c < correct.length) {
-        document.querySelector("#questionbox").innerHTML = question[c];
-        document.querySelector("#a1").innerHTML = answerA[c];
-        document.querySelector("#a2").innerHTML = answerB[c];
-        document.querySelector("#a3").innerHTML = answerC[c];
-        document.querySelector("#a4").innerHTML = answerD[c];
-        correct[b];
-    }
-    c++
-    if (c >= question.length && c >= answerA.length && c >= answerB.length && c >= answerC.length && c >= answerD.length && c >= correct.length) {
-        document.querySelector("#left").style.color = "black";
-    }
-})
+//     if (c < question.length && c < answerA.length && c < answerB.length && c < answerC.length && c < answerD.length && c < correct.length) {
+//         document.querySelector("#questionbox").innerHTML = question[c];
+//         document.querySelector("#a1").innerHTML = answerA[c];
+//         document.querySelector("#a2").innerHTML = answerB[c];
+//         document.querySelector("#a3").innerHTML = answerC[c];
+//         document.querySelector("#a4").innerHTML = answerD[c];
+//         correct[b];
+//     }
+//     if (c >= question.length && c >= answerA.length && c >= answerB.length && c >= answerC.length && c >= answerD.length && c >= correct.length) {
+//         document.querySelector("#left").style.color = "black";
+//         document.querySelector("#left").style.opacity = "0.6";
+//     }
+// })
 
 document.querySelector("#top").addEventListener("click", function () {
-    // runSequence(d)
     document.querySelector(".modal").style.display = "block";
     question = ["Molecule made up of 2 hydrogen atoms and 1 oxygen atom.", "Mother of Blue, Sir, and Rumi.", "sampleQuestion3"];
     answerA = ["What is earth?", "Who is Ciara?", "sampleAnsweri"];
@@ -118,59 +142,91 @@ document.querySelector("#top").addEventListener("click", function () {
         document.querySelector("#a4").innerHTML = answerD[d];
         correct[d];
     }
-    // d++
     if (d >= question.length && d >= answerA.length && d >= answerB.length && d >= answerC.length && d >= answerD.length && d >= correct.length) {
         document.querySelector("#top").style.color = "black";
         document.querySelector("#top").style.opacity = "0.6";
     }
 })
 
-// let runSequence = function (i) {
-//     let choices = document.querySelectorAll(".answerbox")
-//     document.querySelector(".modal").style.display = "block";
+    for (let x = 0; x < choices.length; x++) {
+        choices[x].addEventListener("click", function (event) {
+            const selected = event.target.innerHTML
+            console.log(correct[d])
+            console.log(selected)
+            if (selected === correct[d]) {
+                console.log(score + points);
+                score = score + points
+            }
+            else {
+                console.log(score - points);
+                score = score - points
+            }
+            document.querySelector(".score").innerHTML = score;
+            document.querySelector(".modal").style.display = "none";
+            d++;
+            if (d >= question.length && d >= answerA.length && d >= answerB.length && d >= answerC.length && d >= answerD.length && d >= correct.length) {
+                document.querySelector("#top").style.color = "black";
+                document.querySelector("#top").style.opacity = "0.6";
+                document.querySelector("#top").style.border = "solid black";
+                document.querySelector("#top").addEventListener("click", function () {
+                document.querySelector(".modal").style.display = "none"
+                })
+            }
+        })
+    }
 
-//     for (let x = 0; x < choices.length; x++) {
-//         choices[x].addEventListener("click", function (event) {
-//             const selected = event.target.innerHTML
-//             console.log(correct[i])
-//             console.log(selected)
-//             if (selected === correct[i]) {
-//                 console.log(score + points);
-//                 score = score + points
-//             }
-//             else {
-//                 console.log(score - points);
-//                 score = score - points
-//             }
-//             document.querySelector(".score").innerHTML = score;
-//             document.querySelector(".modal").style.display = "none";
-//         })
-//     }
+
+
+// for (let x = 0; x < choices.length; x++) {
+//     choices[x].addEventListener("click", function (event) {
+//         const selected = event.target.innerHTML
+//         console.log(correct[b])
+//         console.log(selected)
+//         if (selected === correct[b]) {
+//             console.log(score + points);
+//             score = score + points
+//         }
+//         else {
+//             console.log(score - points);
+//             score = score - points
+//         }
+//         document.querySelector(".score").innerHTML = score;
+//         document.querySelector(".modal").style.display = "none";
+//         b++;
+//         if (d >= question.length && d >= answerA.length && d >= answerB.length && d >= answerC.length && d >= answerD.length && d >= correct.length) {
+//             document.querySelector("#bottom").style.color = "black";
+//             document.querySelector("#bottom").style.opacity = "0.6";
+//             document.querySelector("#bottom").style.border = "solid black";
+//             document.querySelector("#bottom").addEventListener("click", function () {
+//                 document.querySelector(".modal").style.display = "none"
+//             })
+//         }
+//     })
 // }
 
-for (let x = 0; x < choices.length; x++) {
-    choices[x].addEventListener("click", function (event) {
-        const selected = event.target.innerHTML
-        console.log(correct[d])
-        console.log(selected)
-        if (selected === correct[d]) {
-            console.log(score + points);
-            score = score + points
-        }
-        else {
-            console.log(score - points);
-            score = score - points
-        }
-        document.querySelector(".score").innerHTML = score;
-        document.querySelector(".modal").style.display = "none";
-        d++;
-        if (d >= question.length && d >= answerA.length && d >= answerB.length && d >= answerC.length && d >= answerD.length && d >= correct.length) {
-            document.querySelector("#top").style.color = "black";
-            document.querySelector("#top").style.opacity = "0.6";
-            document.querySelector("#top").style.border = "solid black";
-            document.querySelector("#top").addEventListener("click", function () {
-                document.querySelector(".modal").style.display = "none"
-            })
-        }
-    })
-}
+// for (let x = 0; x < choices.length; x++) {
+//     choices[x].addEventListener("click", function (event) {
+//         const selected = event.target.innerHTML
+//         console.log(correct[c])
+//         console.log(selected)
+//         if (selected === correct[c]) {
+//             console.log(score + points);
+//             score = score + points
+//         }
+//         else {
+//             console.log(score - points);
+//             score = score - points
+//         }
+//         document.querySelector(".score").innerHTML = score;
+//         document.querySelector(".modal").style.display = "none";
+//         c++;
+//         if (c >= question.length && c >= answerA.length && c >= answerB.length && c >= answerC.length && c >= answerD.length && c >= correct.length) {
+//             document.querySelector("#left").style.color = "black";
+//             document.querySelector("#left").style.opacity = "0.6";
+//             document.querySelector("#left").style.border = "solid black";
+//             document.querySelector("#left").addEventListener("click", function () {
+//                 document.querySelector(".modal").style.display = "none"
+//             })
+//         }
+//     })
+// }
